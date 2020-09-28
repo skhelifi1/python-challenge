d1147f8c5589fd760f555818d7d66fad707da51f
Sirine <sirine.khelifi1@outlook.com>
2020-09-27 20:14:44 -0400

    csv path works now

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,12 @@
 import os
 import csv
-budget_data.csv= os.path.join("..","Resources", "budget_data.csv")
-with open (budget_data.csv, encoding='utf-8') as csvfile:
+budget_data= os.path.join("Resources","budget_data.csv")
+with open (budget_data, encoding='utf-8') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
+    csvreader=next(csvfile)
+       
+Date=[]
+profit_losses=[]
+
+for Total_months in Date:
+    print (Total_months)
