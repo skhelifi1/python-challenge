144df4fc73c9e376ec56d08d38d300390a2a9052
Sirine <sirine.khelifi1@outlook.com>
2020-09-27 23:19:03 -0400

    changed code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,25 @@
 import os
 import csv
 budget_data= os.path.join("Resources","budget_data.csv")
+
+Months=[]
+profit_losses=[]
 with open (budget_data, encoding='utf-8') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
-    csvreader=next(csvfile)
-def Totals (total_numbers):
-    Months= str(total_numbers[0])
-    profit_losses=int(total_numbers[1])
-    Total_months= str(len(Months))
-    Net_Total_Amount= int(len(profit_losses))
-    print(Net_Total_Amount)
-    print(Total_months)
-#for Total_months in Months:
-    #print(len(Total_months))
+    header=next(csvfile)
+    for Months in csvreader:
+        print (Months)
+
+#def Totals (total_numbers):
+    #Months= str(total_numbers[0])
+    #profit_losses=int(total_numbers[1])
+    #Total_months= str(len(Months))
+    #Net_Total_Amount= int(len(profit_losses))
+    #print(Net_Total_Amount)
+    #print(Total_months)
+
+#for row in csvreader:
+    #print(row)
 
 #def Average_changes (total_numbers):
     #Total_months= str(len(Months))
