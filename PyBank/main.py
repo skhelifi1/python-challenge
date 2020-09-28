024d06d419e70f179f3b06e575478c580f27172f
Sirine <sirine.khelifi1@outlook.com>
2020-09-27 22:17:29 -0400

    #avrg code section-no problems-no output

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,15 +9,16 @@ def Totals (total_numbers):
     profit_losses=int(total_numbers[1])
     Total_months= str(len(Months))
     Net_Total_Amount= int(len(profit_losses))
-         
+    print(Net_Total_Amount)
+    print(Total_months)
 #for Total_months in Months:
     #print(len(Total_months))
 
-def Average_changes (total_numbers):
-    Total_months= len(Months)
-    Net_Total_Amount=0
-    for number in profit_losses:
-        Net_Total_Amount+=number
-        return Net_Total_Amount/Total_months
+#def Average_changes (total_numbers):
+    #Total_months= str(len(Months))
+    #Net_Total_Amount=0
+    #for number in profit_losses:
+        #Net_Total_Amount+=number
+        #return Net_Total_Amount/Total_months
 
 
