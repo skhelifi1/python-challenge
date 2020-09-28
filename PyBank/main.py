90e2d9be380c1b36fe947431139c395757333b04
Sirine <sirine.khelifi1@outlook.com>
2020-09-27 22:05:12 -0400

    re-arranged non working code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,20 +5,19 @@ with open (budget_data, encoding='utf-8') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     csvreader=next(csvfile)
 def Totals (total_numbers):
-         Months= str(total_numbers[0])
-         profit_losses=int(total_numbers[1])
-Totals(Months)
+    Months= str(total_numbers[0])
+    profit_losses=int(total_numbers[1])
+    Total_months= str(len(Months))
+    Net_Total_Amount= int(len(profit_losses))
          
-for Total_months in Months:
-    print(len(Total_months))
+#for Total_months in Months:
+    #print(len(Total_months))
 
-Net_Total_Amount= int(len(profit_losses))
-
-#def Average_changes (total_numbers):
-    #length= len(Months)
-    #total=0
-    #for number in profit_losses:
-        #total+=number
-        #return total/length
+def Average_changes (total_numbers):
+    Total_months= len(Months)
+    Net_Total_Amount=0
+    for number in profit_losses:
+        Net_Total_Amount+=number
+        return Net_Total_Amount/Total_months
 
 
