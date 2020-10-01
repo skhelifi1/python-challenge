c5635f6477d7c4d599c270e5e29bbae7dc0235b0
Sirine <sirine.khelifi1@outlook.com>
2020-09-30 23:45:28 -0400

    printing list of candidate part is not working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,23 +5,32 @@ def Totals(election_data):
     total_number_votes=0
     max_votes=661583
     winner=""
+    names=[]
     for row in csvreader:
         total_number_votes+=1
         candidate.append(row[2])
     from collections import Counter
     candidate_count= Counter(candidate)
-    for name,votes in candidate_count.items():
+    for names,votes in candidate_count.items():
         vote_percent= round(100*votes/total_number_votes,3)
-        print(f"{name} {vote_percent}")    
-            
-    print(total_number_votes)
-    print (candidate_count)
-    print(vote_percent)
+        List_candidate=f"[{names}:{vote_percent}]"
+        print(f"[{names}:{vote_percent}]")    
+        print (names)    
+        print(vote_percent)
+        print(total_number_votes)
+        print (candidate_count)
     for name,votes in candidate_count.items():
         if votes==max_votes:
             winner = name
-            max_votes = votes
-
+            
+    Analysis = open("Analysis/Analysis.text", "w")
+    Analysis.write(f"----Election Summary & Results----\n"
+        f"The total number of votes cast is {total_number_votes} \n"
+        f"The candidates who received votes and their corresponding total votes in this election are: {candidate_count}\n"
+        f"The following are the percentages of votes that each candidate has received:{List_candidate} \n"
+        f"The winner of the election based on popular vote is: {winner}\n"
+        f"----------------------------------------------------------------------------------------")
+    Analysis.close()
    
 
 election_data= os.path.join("Resources","election_data.csv")
@@ -38,12 +47,3 @@ with open (election_data, encoding='utf-8') as csvfile:
 #The winner of the election based on popular vote
 #print(f"Election Resultls"
 #print(---------------------------------------------)
-    # Analysis = open("Analysis/Analysis.text", "w")
-    # Analysis.write(f"----Election Summary & Results----\n"
-    # f"The total number of votes cast is {total_number_votes} \n"
-    # f"The candidates who received votes in this election are: {}\n"
-    # f"The following are the percentages of votes that each candidate has received:"{name} {vote_percent}\n"
-    # f"The candidates received the following votes: {}\n"
-    # f"The winner of the election based on popular vote is: {}\n"
-    # f"----------------------------------------------------------------------------------------")
-    # Analysis.close()
